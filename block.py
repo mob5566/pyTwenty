f479f4bee94774c3efcdf7d9706ae102ce8dbde5
code14 <mob5566@gmail.com>
2019-10-20 00:19:54 +0800

    add block functions and initialize blocks

--- block.py
+++ block.py
@@ -1,33 +1,58 @@
 
+import pygame as pg
 import random
+import math
 
 NUM_BLOCKS = 21
-ROW_SIZE = 8
-COL_SIZE = 7
-GAP_WIDTH = 1
+WIDTH_SIZE = 7
+HEIGHT_SIZE = 8
 BLOCK_SIDE = 50
-BLOCK_SHIFT = GAP_WIDTH + BLOCK_SIDE
+BLOCK_DROP_VEL = 10
 
 def genRndBlock(num_lim = NUM_BLOCKS-1):
     if num_lim >= NUM_BLOCKS:
         num_lim = NUM_BLOCKS - 1
 
     num = random.randint(1, num_lim)
-    x = random.randint(0, ROW_SIZE-1)
-    y = random.randint(0, COL_SIZE-1)
+    x = random.randint(0, WIDTH_SIZE-1) * BLOCK_SIDE
+    y = random.randint(0, HEIGHT_SIZE-1) * BLOCK_SIDE
 
     return Block(num, (x, y))
 
 class Block:
     def __init__(self, num, pos):
-        assert 0 <= num and num < NUM_BLOCKS
-        assert 0 <= pos[0] and pos[0] < ROW_SIZE
-        assert 0 <= pos[1] and pos[1] < COL_SIZE
-
         self.num = num
-        self.pos = pos
-        self.x = pos[0]
-        self.y = pos[1]
+        self.rect = pg.Rect(
+            pos[0],
+            pos[1],
+            BLOCK_SIDE,
+            BLOCK_SIDE
+        )
 
-        self.rx = self.x * BLOCK_SHIFT + GAP_WIDTH
-        self.ry = self.y * BLOCK_SHIFT + GAP_WIDTH
+    def isValid(self):
+        if 0 >= self.num or self.num >= NUM_BLOCKS:
+            return False
+        if 0 > self.rect.x or self.rect.x >= WIDTH_SIZE * BLOCK_SIDE:
+            return False
+        if 0 > self.rect.y or self.rect.y >= HEIGHT_SIZE * BLOCK_SIDE:
+            return False
+        return True
+
+    def getPos(self):
+        return (self.rect.x, self.rect.y)
+
+    def getDrop(self):
+        block = self.getMove((0, BLOCK_DROP_VEL))
+        return block
+
+    def getMove(self, roff):
+        block = Block(self.num, (self.rect.x, self.rect.y))
+        block.rect.move_ip(*roff)
+        return block
+
+    def checkCollision(self, block):
+        return self.num != block.num and self.rect.colliderect(block.rect)
+
+    def checkMerge(self, block):
+        center = (block.rect.x + BLOCK_SIDE//2, block.rect.y + BLOCK_SIDE//2)
+        return self.num == block.num and self.rect.collidepoint(center)
--- twenty.py
+++ twenty.py
@@ -13,9 +13,13 @@ PROJ_DIR = path.dirname(path.realpath(sys.argv[0]))
 # plotting settings
 FPS = 60
 TITLE_HEIGHT = 30
+GAME_SIZE = (
+    WIDTH_SIZE * BLOCK_SIDE,
+    HEIGHT_SIZE * BLOCK_SIDE
+)
 SCREEN_SIZE = (
-    COL_SIZE * BLOCK_SHIFT + GAP_WIDTH,
-    ROW_SIZE * BLOCK_SHIFT + GAP_WIDTH
+    GAME_SIZE[0],
+    GAME_SIZE[1] + TITLE_HEIGHT
 )
 QUIT_KEYS = {pg.K_ESCAPE, pg.K_q}
 
@@ -26,9 +30,10 @@ BLOCK_IMGS = []
 DURATION = 5
 
 def main():
-    global screen, fps
+    global screen, fps, game_board
     pg.init()
     screen = pg.display.set_mode(SCREEN_SIZE)
+    game_board = pg.Surface(GAME_SIZE)
     fps = pg.time.Clock()
     pg.display.set_caption('Twenty')
     screen.fill((255, 255, 255))
@@ -60,13 +65,16 @@ def gameMain():
     tw_count = 0
     highest_score = 1
     game_quit = False
-    cur_blocks = dict()
+    cur_blocks = [Block(0, (wid * BLOCK_SIDE, HEIGHT_SIZE * BLOCK_SIDE))
+            for wid in range(WIDTH_SIZE)]
+    init_check = set()
 
     for _ in range(10):
         block = genRndBlock(2)
 
-        if block.pos not in cur_blocks:
-            cur_blocks[block.pos] = block
+        if block.getPos() not in init_check:
+            cur_blocks.append(block)
+            init_check.add(block.getPos())
 
     while True:
         for event in pg.event.get():
@@ -74,7 +82,10 @@ def gameMain():
                 print('Get quit event')
                 game_quit = True
 
+        cur_blocks = updateGame(cur_blocks)
         drawGame(cur_blocks)
+
+        screen.blit(game_board, (0, TITLE_HEIGHT))
 
         if game_quit:
             break
@@ -84,9 +95,49 @@ def gameMain():
 
     return True
 
+def updateGame(blocks):
+    blocks.sort(key=lambda block: block.rect.y, reverse=True)
+    ret_blocks = [block for block in blocks if not block.isValid()]
+    exists = [True] * len(blocks)
+
+    # drop and check collision
+    for i, block in enumerate(blocks):
+        if not block.isValid():
+            continue
+
+        drop_block = block.getDrop()
+        collided = False
+        merged = -1
+
+        for j, nxt_block in enumerate(blocks):
+            if not exists[j] or i == j:
+                continue
+
+            if drop_block.checkCollision(nxt_block):
+                collided = True
+                break
+            elif drop_block.checkMerge(nxt_block):
+                merged = j
+                break
+
+        if collided:
+            ret_blocks.append(block)
+        elif merged >= 0:
+            blocks[merged].num += 1
+            exists[i] = False
+        else:
+            ret_blocks.append(drop_block)
+
+    return ret_blocks
+
 def drawGame(blocks):
-    for block in blocks.values():
-        screen.blit(BLOCK_IMGS[block.num], (block.rx, block.ry))
+    game_board.fill((255, 255, 255))
+
+    for block in blocks:
+        if block.num >= NUM_BLOCKS:
+            continue
+
+        game_board.blit(BLOCK_IMGS[block.num], block.rect)
 
 def gameStartEvent(event):
     start = False
